c942f44fa6a04b82b84c870085ff638b22f002f8
johnp <johnp@LAPTOP-OT80T46V.wireless.dundee.ac.uk>
2019-01-21 12:49:02 +0000

    Initial test cases for Login class

--- src/java/examWorkflow/testLogin.java
+++ src/java/examWorkflow/testLogin.java
@@ -2,13 +2,25 @@ package examWorkflow;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class testLogin {
 
+	public Login login; 
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		login = new Login();
+		login.setUsrname("John");
+		login.setPass("pass");
+	}
+	
 	@Test
-	void test() {
-		
+	public void testGetUsr() {
+		login.setUsrname("d");
+		String username = login.getUsrname();
+		assertNotNull(username);
 	}
 
 }
