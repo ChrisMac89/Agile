6858346a5caa45645ede29347a33d1c83c7d9d6b
Danyal Ali <danyal.ali77@gmail.com>
2019-01-21 13:28:59 +0000

    Update TestConnection.java

--- AC31007Agile/src/java/Agile/TestConnection.java
+++ AC31007Agile/src/java/Agile/TestConnection.java
@@ -5,10 +5,36 @@
  */
 package Agile;
 
+import java.sql.*;
 /**
  *
  * @author Danyal
  */
-public class TestConnection {
+public class landingPage {
     
+    public static void main(String[] args) {
+        
+      try {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+      } catch(ClassNotFoundException e) {
+         System.out.println("Class not found "+ e);
+      }
+      try {
+         Connection con = DriverManager.getConnection(
+            "jdbc:mysql://silva.computing.dundee.ac.uk:3306/18agileteam2db","18agileteam2", "8474.at2.4748");
+         
+         Statement stmt = con.createStatement();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM comments");
+         System.out.println("id  name    job");
+         
+         while (rs.next()) {
+            int id = rs.getInt("recipientId");
+            String name = rs.getString("senderId");
+            String job = rs.getString("message");
+            System.out.println(id+"   "+name+"    "+job);
+         }
+      } catch(SQLException e) {
+         System.out.println("SQL exception occured" + e);
+      }
+   }
 }
