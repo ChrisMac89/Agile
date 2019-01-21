cdca433521cef24d412d13e0a87beec37e4ea853
johnp <johnp@LAPTOP-OT80T46V.wireless.dundee.ac.uk>
2019-01-21 10:55:39 +0000

    Added base logic to the login class

--- src/java/examWorkflow/Login.java
+++ src/java/examWorkflow/Login.java
@@ -5,10 +5,52 @@
  */
 package examWorkflow;
 
-/**
- *
- * @author ChristopherMacleman
- */
+
 public class Login {
-    
+	
+	// variable declarations
+	String usrname;
+	String pass;
+	
+	// constructor to initialise login obj
+	public Login() {
+		usrname = "";
+		pass = "";
+	}
+
+	// getters and setters for variables
+	public String getUsrname() {
+		return usrname;
+	}
+	
+	public String getPass() {
+		return usrname;
+	}
+	
+	public void setUsrname(String x) {
+		usrname = x;
+	}
+	
+	public void setPass(String x) {
+		pass = x;
+	}
+	
+	// query database for username
+	public void queryUsrname(String username) {
+		
+	}
+	
+	// query database for password
+	public void queryPass(String username) {
+		
+	}
+	
+	// verify if 2 passwords match or not 
+	public boolean verifyPass(String password) {
+		if(password == pass) { return true; }
+		else { return false; }
+
+	}
+	
+	
 }
